281783c9f1d0cad471c60fdb69daff2d4b0ea074
Sergio Vidal <sergiovidal1993@gmail.com>
2023-07-21 17:28:40 +0200

    Base

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+//GLOBAL VARIABLES
+const startBtnNode = document.querySelector("#start-btn");
+const cfgBtnNode = document.querySelector("#config-btn");
+const splashScreenNode = document.querySelector("#splash-screen");
+const gameScreenNode = document.querySelector("#game-screen");
+const endScreenNode = document.querySelector("#end-screen");
+
+//STATE MANAGENT FUNCTIONS
+function startGame() {
+  console.log("iniciando el juego");
+  splashScreenNode.style.display = "none";
+  gameScreenNode.style.display = "flex";
+}
+
+function cfgMenu() {
+  console.log("Entrando a configuracion");
+}
+
+//ADD EVENT LISTENERS
+
+startBtnNode.addEventListener("click", startGame);
+cfgBtnNode.addEventListener("click", cfgMenu);
